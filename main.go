9fb07ffbe7a09c65628c07b5bb6002fbc2a64bee
Gabriel Dalay <gdalay@jibe.com>
2016-01-23 08:05:39 -0500

    Updated README.md

--- main.go
+++ main.go
@@ -1,5 +1,79 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/jebrial/learnlink/models"
+	"golang.org/x/net/context"
+	"log"
+	"net/http"
+	"os"
+)
 
+type ContextHandler interface {
+	ServeHTTPContext(context.Context, http.ResponseWriter, *http.Request)
+}
+
+type ContextHandlerFunc func(context.Context, http.ResponseWriter, *http.Request)
+
+func (h ContextHandlerFunc) ServeHTTPContext(ctx context.Context, rw http.ResponseWriter, req *http.Request) {
+	h(ctx, rw, req)
+}
+
+type ContextAdapter struct {
+	ctx     context.Context
+	handler ContextHandler
+}
+
+func (ca *ContextAdapter) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	ca.handler.ServeHTTPContext(ca.ctx, rw, req)
+}
+
+type config struct {
+	dbUrl string
+}
+
+func main() {
+	//load the config
+	file, err := os.Open("config.json")
+	if err != nil {
+		log.Panic(err)
+	}
+	decoder := json.NewDecoder(file)
+	config := config{}
+	err = decoder.Decode(&config)
+	if err != nil {
+		log.Panic(err)
+	}
+	// connect to the database
+	db, err2 := models.NewDB(config.dbUrl)
+	if err2 != nil {
+		log.Panic(err2)
+	}
+
+	ctx := context.WithValue(context.Background(), "db", db)
+
+	http.Handle("/users", &ContextAdapter{ctx, ContextHandlerFunc(usersIndex)})
+	err = http.ListenAndServe(":3001", nil)
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func usersIndex(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(405), 405)
+		return
+	}
+
+	users, err := models.AllUsers(ctx)
+	if err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	for _, user := range users {
+		fmt.Fprintf(w, "%d, %s, %s\n", user.Id, user.Name, user.Email)
+	}
 }
--- models/user.go
+++ models/user.go
@@ -1,5 +1,13 @@
 package models
 
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"golang.org/x/net/context"
+	"time"
+)
+
 type User struct {
 	Id        int
 	Name      string
@@ -7,3 +15,44 @@ type User struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
+
+func AllUsers(ctx context.Context) ([]*User, error) {
+	db, ok := ctx.Value("db").(*sql.DB)
+	if !ok {
+		return nil, errors.New("Models: could not get database connection to pool")
+	}
+	rows, err := db.Query("SELECT * FROM classmate")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	users := make([]*User, 0)
+	for rows.Next() {
+		user := new(User)
+		err := rows.Scan(&user.Id, &user.Name, &user.Email, &user.CreatedAt, &user.UpdatedAt)
+		fmt.Println(user)
+		if err != nil {
+			fmt.Println(err)
+			return nil, err
+		}
+		users = append(users, user)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
+/*
+CREATE TABLE "classmate" (
+	id bigserial primary key,
+	name varchar(50) NOT NULL,
+	email varchar(50) NOT NULL,
+	created_at timestamp DEFAULT current_timestamp,
+	updated_at timestamp DEFAULT current_timestamp
+);
+
+INSERT INTO "classmate" (name,email) VALUES ('rick','plumbus@fleeb.com'), ('morty', 'dumbus@fleeb.com');
+
+*/
