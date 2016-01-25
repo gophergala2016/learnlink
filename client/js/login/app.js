4f7b16920d02a36d310f5031dcc39e9929f78b50
Howard Kim <howardlykim@Howards-MBP-2.fios-router.home>
2016-01-24 19:52:23 -0500

    Wrapping up Login module: user can now log-in or sign-up for an account.

--- client/js/login/app.js
+++ client/js/login/app.js
@@ -1,12 +1,41 @@
 angular
-  .module('LEARNLINK_LOGIN', ['ngRoute'])
-  .controller('LoginCtrl', function ($scope) {
+  .module('LEARNLINK_LOGIN', ['ngRoute', 'ngCookies'])
+  .service('LoginService', function ($http) {
+
+    return {
+      login: function (user_info) {
+        return $http.post('/login', user_info);
+      },
+      signup: function (user_info) {
+        return $http.post('/signup', user_info);
+      }
+    };
+
+  })
+  .controller('LoginCtrl', function ($scope, $cookies, LoginService) {
+
+    $scope.loginType = 'login';
+
     $scope.user = {
       username: undefined,
       password: undefined,
       email: undefined
     };
-    $scope.submit = function (user) {
-      console.log(user);
+
+    $scope.changeLoginType = function (login_type) {
+      $scope.loginType = login_type;
     };
+
+    $scope.submit = function (user, login_type) {
+
+      LoginService[login_type](user)
+        .then(function (res) {
+          $cookies.put('token', res.token);
+          window.user = res.user;
+        }, function (err) {
+          alert('Something occurred!', err.toString());
+        });
+
+    };
+
   });
